611a5bac8a3c96e142218dd8846f2f298c4ba3bc
Mateus Koppe <mateus.koppe@gmail.com>
2020-12-21 11:28:40 -0300

    Git init

---
+++ src/test/java/com/memcache/sim/AppTest.java
@@ -0,0 +1,24 @@
+package com.memcache.sim;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.memcache.sim.App;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
